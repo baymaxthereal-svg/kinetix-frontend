badc2a169263340af898334634e393b14b5be6d3
fahad iftikhar <fahad@fvinnovation.com>
2026-04-16 17:58:20 +0500

    add dashboard-login-page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './common/navbar';
 import Footer from './common/footer';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './components/home';
 import BrowseCetegories from './components/browseCetegories';
 import BulkOrder from './components/bulkOrder';
 import ProductDetail from './components/productDetail';
 import ProductList from './components/productList';
 import Cart from './components/cart';
-import About from './components/about'
+import About from './components/about';
+import DashboardLogin from './dashboard/login';
+
+// Layout for public pages (with Navbar + Footer)
+function PublicLayout({ children }) {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+}
+
+// Layout for separate pages (without Navbar/Footer)
+function SeparateLayout({ children }) {
+  return <>{children}</>;
+}
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Navbar />
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/Categories" element={<BrowseCetegories />} />
-          <Route path="/BulkOrder" element={<BulkOrder />} />
-          <Route path="/ProductDetail" element={<ProductDetail />} />
-          <Route path="/ProductList" element={<ProductList />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/Cart" element={<Cart />} />
+          {/* Public routes - use PublicLayout */}
+          <Route path="/" element={<PublicLayout><HomePage /></PublicLayout>} />
+          <Route path="/Categories" element={<PublicLayout><BrowseCetegories /></PublicLayout>} />
+          <Route path="/BulkOrder" element={<PublicLayout><BulkOrder /></PublicLayout>} />
+          <Route path="/ProductDetail" element={<PublicLayout><ProductDetail /></PublicLayout>} />
+          <Route path="/ProductList" element={<PublicLayout><ProductList /></PublicLayout>} />
+          <Route path="/about" element={<PublicLayout><About /></PublicLayout>} />
+          <Route path="/Cart" element={<PublicLayout><Cart /></PublicLayout>} />
+
+          {/* Separate routes - no navbar/footer */}
+          <Route path="/dashboard" element={<SeparateLayout><DashboardLogin /></SeparateLayout>} />
         </Routes>
-        <Footer />
       </div>
     </BrowserRouter>
   );
 }
+
 export default App;
